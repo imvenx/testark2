0a2d48ec4a2b3a7b8e6c1f134a4b1da370bc2113
imv <imvenx@proton.me>
2023-02-04 15:55:34 +0100

    test

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <router-view /> -->
+  <div>isAbsolute: {{ isAbsolute }}</div>
+  <div>isSupported: {{ isSupported }}</div>
+  <div>alpha: {{ alpha }}</div>
+  <div>beta: {{ beta }}</div>
+  <div>gamma: {{ gamma }}</div>
+</template>
+
+<script setup lang="ts">
+import { useDeviceOrientation } from '@vueuse/core'
+
+const {
+  isAbsolute,
+  isSupported,
+  alpha,
+  beta,
+  gamma,
+} = useDeviceOrientation()
+</script>
+
+<style>
+* {
+  text-align: center;
+  color: greenyellow;
+  background-color: black;
+  font-size: larger;
+}
+</style>
