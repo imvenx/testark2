5094dfc7fda9a9ea5e23efbcf899fc2438435cf5
imv <imvenx@proton.me>
2023-02-04 16:18:00 +0100

    asd

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
   <!-- <router-view /> -->
   <div>isAbsolute: {{ isAbsolute }}</div>
   <div>isSupported: {{ isSupported }}</div>
-  <div>alpha: {{ alpha }}</div>
-  <div>beta: {{ beta }}</div>
-  <div>gamma: {{ gamma }}</div>
+  <div>alpha: {{ alpha?.toFixed(3) }}</div>
+  <div>beta: {{ beta?.toFixed(3) }}</div>
+  <div>gamma: {{ gamma?.toFixed(3) }}</div>
+
+  <div>acceleration: {{ acceleration }}</div>
+  <div>accelerationIncludingGravity: {{ accelerationIncludingGravity }}</div>
+  <div>rotationRate: {{ rotationRate }}</div>
+  <div>interval: {{ interval?.toFixed(3) }}</div>
 </template>
 
 <script setup lang="ts">
 import { useDeviceOrientation } from '@vueuse/core'
+import { useDeviceMotion } from '@vueuse/core'
 
 const {
   isAbsolute,
@@ -17,6 +23,14 @@ const {
   beta,
   gamma,
 } = useDeviceOrientation()
+
+
+const {
+  acceleration,
+  accelerationIncludingGravity,
+  rotationRate,
+  interval,
+} = useDeviceMotion()
 </script>
 
 <style>
